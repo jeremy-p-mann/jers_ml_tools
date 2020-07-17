3c142a23b97e016af2ebcc07897441ff6db09c50
Jeremy Mann <jmann277@gmail.com>
2020-07-16 17:29:45 -0700

    add bettier docs

--- jerml/transformers/bettier.py
+++ jerml/transformers/bettier.py
@@ -1,24 +1,39 @@
 # Gets betti numbers from images
 
-from homcv.betti_numbers import betti_numbers
+from homcv import betti_numbers
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class Bettier(BaseEstimator, TransformerMixin):
-  '''
-  Computes the Betti Numbers of a batch of images
+    '''
+    Computes the Betti Numbers of the dark regions of a batch of images
 
-  '''
+    Attributes:
+    -----------
+    threshold: float, optional
+        The transform method computes the Betti numbers of the region 
+        formed by any pixel darker than `threshold`.
+    '''
+    def __init__(self, threshold=.5):
+        self.threshold = threshold
 
-  def __init__(self, threshold=.5):
-    self.threshold = threshold
+    def fit(self, X, y=None):
+        return self
 
-  def fit(self, X, y=None):
-    return self
+    def transform(self, X, y=None):
+        '''
+        Returns the betti numbers of the dark region of the images.
+        
+        Inputs
+        ------
+        X: ndarray, shape (n_samples, n_x, n_y)
+            Batch of grayscale images.
+        
+        Returns
+        -------
+        X_transformed: ndarry, shape (n_samples, 2)
+            Zeroeth and first Betti numbers of each image in the batch
+        '''
+        X_transformed = None
+        return X_transformed
 
-  def _get_betti_numbers(self, X):
-    pass
-
-  def transform(self, X, y=None):
-    betti_numbers = None
-    return None
