01f4e673d841efd83444b4bb0c9420d398fff1c5
Jeremy Mann <jmann277@gmail.com>
2020-07-20 11:57:28 -0700

    add RF hyperparameter script

---
+++ jerml/hyperparameters.py
@@ -0,0 +1,21 @@
+''' 
+Generates hyperparameters for various ML models
+'''
+
+
+class RandomForestHyperparameters:
+    '''
+    Generates hyperparameters for Random Forrest
+    '''
+    def __init__(self):
+        pass
+
+    def generate_hyperparameters(self, n_hyperparams):
+        '''
+        Generates a dictionary of hyperparameters
+        '''
+
+        hyperparam_dict = {}
+
+        pass
+    
